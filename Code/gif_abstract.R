c96f7bf3a06d4109dfe843de1b4400beb8f70a74
duncanobrien <duncan.a.obrien@gmail.com>
2022-12-30 11:47:09 +0000

    add gif to repo

--- Code/gif_abstract.R
+++ Code/gif_abstract.R
@@ -29,11 +29,15 @@ id_data <- rbind(did_id_data, hom_id_data, prey_id_data) %>%
 cond_inter_treatment2 <- conditional_effects(brms_lm,effects = "mean_length_um:treatment",
                                              conditions = data.frame(expand.grid(time_point = unique(id_data$time_point),
                                                                                  predator_treatment =  c("prey","didinium","homalozoon"))))[[1]]|>
-  mutate(cond__ = paste(time_point,predator_treatment,sep="_"),
+  mutate(cond__ = ifelse(str_length(time_point) == 1,
+                         paste0("  ",time_point,"h"),
+                         paste0(time_point,"h")),
          effect = "interaction")
 
+unlink("Results/abstract_gif/gif1", recursive = TRUE) # remove old version of gif
+
 camcorder::gg_record(
-  dir = file.path("/Users/ul20791/Downloads/camcorder_test", "recording100"), # where to save the recording
+  dir = file.path("Results/abstract_gif", "gif1"), # where to save the recording
   device = "png", # device to use to save images
   width = 5,      # width of saved image
   height = 2,     # height of saved image
@@ -44,12 +48,11 @@ camcorder::gg_record(
 for(i in unique(cond_inter_treatment2$time_point)){
   print(
     ggplot(cond_inter_treatment2 |>
-           mutate(cond__ = paste0(time_point,"h")) |>
-           filter(cond__ == paste0(i,"h")) |>
+           filter(time_point == i) |>
       mutate(predator_treatment = factor(predator_treatment,labels =  c("Control","Didinium","Homalozoon"))))+
     geom_line(aes(x = effect1__, y=estimate__,col=as.factor(effect2__))) +
     geom_ribbon(aes(x = effect1__,ymin = lower__, ymax =  upper__,fill=as.factor(effect2__)),alpha=0.5)+
-    facet_grid(cond__~predator_treatment, scales = "fixed") +
+    facet_grid(cond__~predator_treatment) +
     scale_fill_manual(name = "Temperature \u00B0C",values = c("#a2d7d8","#de5842")) + 
     scale_color_manual(name = "Temperature \u00B0C",values = c("#a2d7d8","#de5842")) + 
     xlab("Mean length (\u03BCm)")+
@@ -58,19 +61,21 @@ for(i in unique(cond_inter_treatment2$time_point)){
     theme_classic()+
     theme(strip.background = element_rect(colour = "black", fill = "white", linetype = "blank"),
           strip.text = element_text(face = "bold"),
-          strip.text.y.right = element_text(angle = 0),
-          strip.text.x = element_text(face = "bold.italic"),
+          strip.text.y.right = element_text(angle = 0,margin = margin(l=2, r = 2)),
+          strip.text.x = element_text(face = "bold.italic" ),
           panel.background = element_blank(),
           axis.line = element_line(colour = "black"),
           panel.border = element_rect(fill = NA, colour = "black"))
   )
 }
+
 camcorder::gg_playback(
-  name = file.path("/Users/ul20791/Downloads/camcorder_test","vignette_gif.gif"),
+  name = file.path("Results/abstract_gif","gif1.gif"),
   first_image_duration = 5,
   last_image_duration = 5,
   frame_duration = 0.25,
-  last_as_first = FALSE
+  last_as_first = FALSE,
+  background = "none"
 )
 
 camcorder::gg_stop_recording()
