5ecd4b6e4ed74b90a9e1661336732fcfd457dd0e
duncanobrien <duncan.a.obrien@gmail.com>
2023-03-09 10:18:55 +0000

    fixed supplementary rounding errors

--- Code/TablesS1_2.R
+++ Code/TablesS1_2.R
@@ -15,17 +15,21 @@ brms_lm_id <- readRDS(file = "Results/brms_lm_id_noar.rds")
 speed_model_summary_table <- data.frame(rbind(summary(brms_id_weibullk8)$random[[1]],
                                               summary(brms_id_weibullk8)$fixed,
                                               summary(brms_id_weibullk8)$splines)) %>%
+  dplyr::mutate(dplyr::across(Estimate:Tail_ESS,~as.numeric(.))) %>% #remove tibble's pillar_number formatting
   tibble::rownames_to_column("Parameter") %>%
-  dplyr::mutate(dplyr::across(Estimate:Tail_ESS,~signif(.,digit=3))) %>%
-  dplyr::add_row(Parameter = "random",.before = 1) %>%
+  dplyr::mutate(across(Estimate:u.95..CI,~round(.,digits =3))) %>%
+  dplyr::mutate(across(Rhat:Tail_ESS,~round(.,digits = 2))) %>%
+  dplyr::add_row(Parameter = "random",.before = 1) %>% #format table into sections (random effects, fixed effects & splines)
   dplyr::add_row(Parameter = "fixed",.before = 3) %>%
   dplyr::add_row(Parameter = "splines",.before = 22) 
 
 shape_model_summary_table <- data.frame(rbind(summary(brms_lm_id)$random[[1]],
                                               summary(brms_lm_id)$fixed,
                                               summary(brms_lm_id)$splines)) %>%
+  dplyr::mutate(dplyr::across(Estimate:Tail_ESS,~as.numeric(.))) %>% #remove tibble's pillar_number formatting
   tibble::rownames_to_column("Parameter") %>%
-  dplyr::mutate(dplyr::across(Estimate:Tail_ESS,~signif(.,digit=3))) %>%
+  dplyr::mutate(across(Estimate:u.95..CI,~round(.,digits =3))) %>%
+  dplyr::mutate(across(Rhat:Tail_ESS,~round(.,digits = 2))) %>%
   dplyr::add_row(Parameter = "random",.before = 1) %>%
   dplyr::add_row(Parameter = "fixed",.before = 3)
 
@@ -33,8 +37,8 @@ shape_model_summary_table <- data.frame(rbind(summary(brms_lm_id)$random[[1]],
 #Save tables
 ############ 
 
-readr::write_csv(speed_model_summary_table,
-                 file = "Results/figures/supplementary_figures/speed_model_summary_table.csv")
+write.csv(speed_model_summary_table,
+                 file = "Results/figures/supplementary_figures/speed_model_summary_table.csv", row.names = FALSE)
 
-readr::write_csv(shape_model_summary_table,
-                 file = "Results/figures/supplementary_figures/shape_model_summary_table.csv")
+write.csv(shape_model_summary_table,
+                 file = "Results/figures/supplementary_figures/shape_model_summary_table.csv", row.names = FALSE)
